104cf328eb0f9df32aab81b9dc8a772a84c8939d
khalil Frough <khalil.froughkmu@gmail.com>
2020-06-12 22:57:21 +1000

    fixed the bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,20 +3,5 @@ class HomeController < ApplicationController
     @cities = City.all
   end
 
-  def difference
-    
-  end 
-
-  def findCity
-    @city=City.find(params[:id])
-  end 
-
-  def local (*args)
-    localTime=Time.utc(*args)
-    ActiveSupport::TimeWithZone.new(nil,self,localTime)
-  end 
-
-  def today
-    tzinfo.now.to_date
-  end 
+ 
 end
